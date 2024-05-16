4cf52bb696e21740ad92ffecc7d24d47c2fff039
usktea <tjrxo1234@gmail.com>
2024-05-16 23:26:15 +0900

    test

--- src/test/java/com/pomodoro/pomodoromate/PomodoroMateApplicationTests.java
+++ src/test/java/com/pomodoro/pomodoromate/PomodoroMateApplicationTests.java
@@ -1,18 +1,21 @@
 package com.pomodoro.pomodoromate;
 
+
 import com.pomodoro.pomodoromate.config.GoogleConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @ContextConfiguration(classes = {GoogleConfig.class})
+@TestPropertySource(locations = "classpath:application.properties")
 class PomodoroMateApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

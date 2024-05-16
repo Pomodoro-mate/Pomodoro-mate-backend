f0b1c350fc8782dd42fa9df369b3d6bc9b4fc2e4
junhyeong00 <107493122+junhyeong00@users.noreply.github.com>
2024-05-16 23:10:32 +0900

    Fix: test에 google config 의존성 추가

--- src/test/java/com/pomodoro/pomodoromate/participant/applications/ParticipateServiceIntegrationTest.java
+++ src/test/java/com/pomodoro/pomodoromate/participant/applications/ParticipateServiceIntegrationTest.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.context.annotation.Import;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;

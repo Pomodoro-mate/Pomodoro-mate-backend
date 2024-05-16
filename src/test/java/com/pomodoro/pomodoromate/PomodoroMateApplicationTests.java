5fa26ace0a8a0b7598b18496bffb059973e4c894
usktea <tjrxo1234@gmail.com>
2024-05-16 23:38:19 +0900

    Fix config

--- src/test/java/com/pomodoro/pomodoromate/PomodoroMateApplicationTests.java
+++ src/test/java/com/pomodoro/pomodoromate/PomodoroMateApplicationTests.java
@@ -1,16 +1,12 @@
 package com.pomodoro.pomodoromate;
 
-
-import com.pomodoro.pomodoromate.auth.config.GoogleConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@ContextConfiguration(classes = {GoogleConfig.class})
 @TestPropertySource(locations = "classpath:application.properties")
 class PomodoroMateApplicationTests {
 

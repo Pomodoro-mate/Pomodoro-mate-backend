e12907a11cd676bfaae606181e01b25fe193dda6
usktea <tjrxo1234@gmail.com>
2024-05-16 23:35:44 +0900

    test

--- src/test/java/com/pomodoro/pomodoromate/participant/applications/ParticipateServiceIntegrationTest.java
+++ src/test/java/com/pomodoro/pomodoromate/participant/applications/ParticipateServiceIntegrationTest.java
@@ -17,7 +17,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;

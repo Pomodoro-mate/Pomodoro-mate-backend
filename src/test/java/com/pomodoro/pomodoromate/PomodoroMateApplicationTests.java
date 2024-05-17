b55cb71c0c2d0532a25ab57df09123c6e941a568
junhyeong00 <jhbae0420@naver.com>
2024-05-17 22:56:44 +0900

    Fix: spring boot test에 properties 추가

--- src/test/java/com/pomodoro/pomodoromate/PomodoroMateApplicationTests.java
+++ src/test/java/com/pomodoro/pomodoromate/PomodoroMateApplicationTests.java
@@ -1,18 +1,15 @@
 package com.pomodoro.pomodoromate;
 
-import com.pomodoro.pomodoromate.config.GoogleConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@SpringBootTest(classes = PomodoroMateApplicationTests.class)
 @ActiveProfiles("test")
-@ContextConfiguration(classes = {GoogleConfig.class})
 class PomodoroMateApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

3735d00608f4b279f1e16e1a80ff70b0c921704a
OHJINSEONG <ojseong0828@naver.com>
2024-05-18 17:44:11 +0900

    Fix : 브런치 충돌 해결
    test에 @contextconfiguration 지우기

--- src/test/java/com/pomodoro/pomodoromate/PomodoroMateApplicationTests.java
+++ src/test/java/com/pomodoro/pomodoromate/PomodoroMateApplicationTests.java
@@ -1,15 +1,12 @@
 package com.pomodoro.pomodoromate;
 
 
-import com.pomodoro.pomodoromate.auth.config.GoogleConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@ContextConfiguration(classes = {GoogleConfig.class})
 class PomodoroMateApplicationTests {
 
     @Test

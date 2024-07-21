eafb30641f51f62a9b5e823aa9584d8fdb1cbac4
junhyeong00 <jhbae0420@naver.com>
2024-07-21 10:57:17 +0900

    Fix: Nginx 적용 후 토큰 재발급이 안되는 이슈 해결

--- src/main/java/com/pomodoro/pomodoromate/common/exceptions/ExceptionResponse.java
+++ src/main/java/com/pomodoro/pomodoromate/common/exceptions/ExceptionResponse.java
@@ -4,4 +4,11 @@ public record ExceptionResponse(String message) {
     public static ExceptionResponse of (String message){
         return new ExceptionResponse(message);
     }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "\"message\": \"" + message + "\"" +
+                "}";
+    }
 }
